e2823639146af5d53a0f8a9a05d44d2d2ccf0979
John T. Saxon <me@sacko.uk>
2017-09-10 19:36:11 +0100

    Snapshot.

--- cmake/Modules/FindLeptonica.cmake
+++ cmake/Modules/FindLeptonica.cmake
@@ -4,12 +4,12 @@ libfind_pkg_check_modules(Leptonica_PKGCONF lept)
 
 find_path(Leptonica_INCLUDE_DIR
   NAMES leptonica/allheaders.h
-  PATHS ${Leptonica_PKGCONF_INCLUDE_DIRS}
+  PATHS ${Leptonica_PKGCONF_INCLUDE_DIRS} ${CMAKE_INCLUDE_PATH}
 )
 
 find_library(Leptonica_LIBRARY
   NAMES lept
-  PATHS ${Leptonica_PKGCONF_LIBRARY_DIRS}
+  PATHS ${Leptonica_PKGCONF_LIBRARY_DIRS} ${CMAKE_LIBRARY_PATH}
 )
 
 set(Leptonica_PROCESS_INCLUDES Leptonica_INCLUDE_DIR)
